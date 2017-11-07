0223cfea25d48c1350b4f0183108bdb5066c7e65
mukeshpalad <mukesh.pal@appdirect.com>
2017-11-07 14:36:09 +0530

    first commit

---
+++ src/main/java/com/tech/challange/techchallange/TechChallangeApplication.java
@@ -0,0 +1,12 @@
+package com.tech.challange.techchallange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallangeApplication.class, args);
+	}
+}
